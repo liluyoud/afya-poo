eb86ee20e1ed3001f0956406d2c4c0043d609c57
Liluyoud Cury de Lacerda <liluyoud@mpro.mp.br>
2026-02-12 16:36:35 -0400

    criar menu

--- Aula01/Program.cs
+++ Aula01/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Clear();
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Clear();
+ResetColor();
+
+WriteLine("-------------------------------");
+WriteLine("     Primeiro Programa C#     ");
+WriteLine("-------------------------------");
+WriteLine();
+WriteLine(" [1] Dizer Alo, Mundo!");
+WriteLine(" [2] Calcular média!");
+WriteLine(" [3] Mostrar o dobro de um número!");
+WriteLine(" [4] Dizer se é par ou impar!");
+WriteLine();
+WriteLine("-------------------------------");
+
+ForegroundColor = ConsoleColor.Yellow;
+Write(" Selecione uma opção: ");
+var opcao = ReadLine();
+ResetColor();
+
+WriteLine("-------------------------------");
+WriteLine("\n Você escolheu a opção: " + opcao);
+ReadLine();
--- Aula01/obj/Debug/net10.0/Aula01.AssemblyInfo.cs
+++ Aula01/obj/Debug/net10.0/Aula01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc24388436a8069cb85157235c91a2b9a2a210f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
